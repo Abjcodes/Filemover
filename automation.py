920916c6e7cbd2c706f30d4b1717f74b67ffbcda
Abijith mv <abijithmv01@gmail.com>
2020-03-21 18:45:07 +0300

    Initial commit

---
+++ automation.py
@@ -0,0 +1,43 @@
+import shutil
+import os
+import time
+
+path = r"C:/Users/user/Desktop/ts"
+
+destinations = {
+    'dpdf': r"C:\Users\user\Desktop\Ebook",
+    'dmp3': r"C:\Users\user\Desktop\MUSIC",
+    'dmkv': r"C:\Users\user\Desktop\flims",
+    'djpg': r"C:\Users\user\Desktop\Widget Code",
+}
+
+
+def filemover():
+    for folders, subfolders, filenames in os.walk(path):
+        for filename in filenames:
+            if filename.endswith('{}'.format('.pdf')):
+                shutil.move(os.path.join(folders, filename), os.path.join(destinations['dpdf'], filename))
+                print('Moving done!!')
+
+            elif filename.endswith('{}'.format('.mp3')):
+                shutil.move(os.path.join(folders, filename), os.path.join(destinations['dmp3'], filename))
+
+                print('Moving done!!')
+
+            elif filename.endswith('{}'.format('.mp4')):
+                shutil.move(os.path.join(folders, filename), os.path.join(destinations['dmkv'], filename))
+
+                print('Moving done!!')
+
+            elif filename.endswith('{}'.format('.jpg')):
+                shutil.move(os.path.join(folders, filename), os.path.join(destinations['djpg'], filename))
+
+                print('Moving done!!')
+
+            else:
+                print("Nothing to move")
+
+
+while True:
+    filemover()
+    time.sleep(5)
